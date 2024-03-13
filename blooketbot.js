4d99e28b6eda4a09bdac0a36f61c6af4cc8702e8
Glitch (glitch-hello-node) <none>
2024-03-13 02:17:18 +0000

    🎭⛩ Checkpoint ./blooketbot.js:49854295/3723 ./package.json:49854295/236 ./index.html:49854295/2283 ./script.js:49854295/8594 ./style.css:49854295/481432

---
+++ blooketbot.js
@@ -0,0 +1,96 @@
+const { initializeApp, deleteApp } = require('firebase/app');
+const { getAuth, signInWithCustomToken, setPersistence } = require('firebase/auth');
+const { getDatabase, ref, set, get, onValue } = require('firebase/database');
+const request = require('request-promise');
+var express = require("express");
+const path = require("path");
+const WebSocket = require("ws");
+const app = express();
+var game = "5327848";
+var n = "Im not hacking!";
+request.get("https://play.blooket.com/play",function(a,resp,c){
+global.cookiew = resp.headers["set-cookie"][0].split(";")[0];
+console.log("Blooket Cookie Ready!");
+});
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+app.get("/script.js", function(req, res) {
+  res.sendFile(path.join(__dirname, "/script.js"));
+});
+app.get("/style.css", function(req, res) {
+  res.sendFile(path.join(__dirname, "/style.css"));
+});
+const webserver = app.listen(80, function() {
+  console.log('Webserver started on port 80!');
+});
+let wss = new WebSocket.Server({ noServer: true });
+webserver.on('upgrade', (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, socket => {
+    wss.emit('connection', socket, request);
+  });
+});
+//wss msg format: {c:"control string",v:"value"}
+wss.on('connection', function(ws, req) {
+ws.send(JSON.stringify({c:"con"}));
+console.log("Client connected!");
+ws.sendJson = function(data){this.send(JSON.stringify(data));}
+ws.log = function(msg,lvl){this.send(JSON.stringify({c:"log",l:lvl,v:msg}));}
+ws.on('message', function(msg){
+var control = JSON.parse(msg);
+switch(control.c){
+case "join":
+if(ws.liveApp){deleteApp(ws.liveApp);ws.liveApp=false;}
+connectToGame(control.gc,control.name,ws).then(e=>{if(ws.db){ws.sendJson({c:"bc",v:true});}});
+break;
+case "sv":
+if(!ws.db){return;}
+try{setVal(ws.db,control.val);}catch(e){ws.log("Error: "+e,"error");}
+break;
+case "leave":
+if(ws.liveApp){deleteApp(ws.liveApp);ws.liveApp=false;ws.sendJson({c:"bc",v:false});}
+break;
+}
+});
+ws.on('close', function(){
+console.log("Client disconnected!");
+if(ws.liveApp){
+deleteApp(ws.liveApp);ws.liveApp=false;}
+});
+});
+//control stat is {c:"log",l:"loglevel",v:"logmessage"}
+//bot connect is c:"bc"
+//bot disconnect is c:"bdc"
+async function connectToGame(gid,name,ws){
+const body = await request.put({body:{id:gid,name:name},json:true,uri:"https://fb.blooket.com/c/firebase/join",headers:{Cookie:global.cookiew}});
+if(!body.success){ws.log("Connect Error: " + body.msg, "error");return;}
+try{
+var db = await connect(body,gid,name,ws);
+ws.db = db;
+onValue(ref(db,gid),(msg)=>{var data = msg.val();ws.sendJson({c:"update",v:data});});
+}
+catch(e){ws.log("Error: " + e, "error");return;}
+}
+async function connect(body,gameid,name,ws)
+{		
+		const liveApp = initializeApp({
+                        apiKey: "AIzaSyCA-cTOnX19f6LFnDVVsHXya3k6ByP_MnU",
+                        authDomain: "blooket-2020.firebaseapp.com",
+                        projectId: "blooket-2020",
+                        storageBucket: "blooket-2020.appspot.com",
+                        messagingSenderId: "741533559105",
+                        appId: "1:741533559105:web:b8cbb10e6123f2913519c0",
+                        measurementId: "G-S3H5NGN10Z",
+                        databaseURL: body.fbShardURL
+                    }, name);
+                    const auth = getAuth(liveApp);
+                    await signInWithCustomToken(auth, body.fbToken);
+                    const db =  getDatabase(liveApp);
+		    ws.liveApp = liveApp;
+await set(ref(db,`${gameid}/c/${name}`),{b:"Rainbow Astronaut",bg:"fire"});
+return db;
+}
+function setVal(db,value){
+set(ref(db,value.path),value.val);
+}
+//use setval like setVal(db,{path:"c/name",val:{b:"Rainbow Astronaut"}})
---
+++ script.js
@@ -0,0 +1,154 @@
+var ws = new WebSocket("ws://localhost");
+var botinfo = {};
+var gameobject = {};
+var cheats = {"Hack":[{
+type:"button",name:"Crash host(crypto)",action:function(a){setUserVal("cr/t","t");a.innerText="Crashing";}
+},{
+type:"button",name:"Freeze Scoreboard",action:function(a){if(a.frozen!=undefined){a.frozen=!a.frozen;}else{a.frozen=true;}if(a.frozen){setUserVal("tat/t","t");}else{setUserVal("tat","t");}a.innerText=a.frozen?"Unfreeze Scoreboard":"Freeze Scoreboard";}
+},{
+type:"input",name:"Set Crypto",action:function(amt){setUserVal("cr",amt);}
+},{
+type:"input",name:"Set Password",action:function(val){setUserVal("p",val);}
+},{
+type:"select",name:"Steal Crypto From",computed:function(sel){if(Object.keys(gameobject.c).length===Array.from(sel.children).length){return false;}return Object.keys(gameobject.c);},action:function(d){setUserVal("tat",`${d}:${prompt("How much crypto do you want to steal?")}`);}
+}],"Gold":[{
+type:"button",name:"Crash host(gold)",action:function(a){setUserVal("g/t","t");a.innerText="Crashing";}
+},{
+type:"button",name:"Freeze Scoreboard",action:function(a){if(a.frozen!=undefined){a.frozen=!a.frozen;}else{a.frozen=true;}if(a.frozen){setUserVal("tat/t","t");}else{setUserVal("tat","t");}a.innerText=a.frozen?"Unfreeze Scoreboard":"Freeze Scoreboard";}
+},{
+type:"select",name:"Steal Gold From",computed:function(sel){if(Object.keys(gameobject.c).length===Array.from(sel.children).length){return false;}return Object.keys(gameobject.c);},action:function(d){setUserVal("tat",`${d}:${prompt("How much gold do you want to steal?")}`);}
+},
+{
+type:"select",name:"Set Player's Gold",computed:function(sel){if(Object.keys(gameobject.c).length===Array.from(sel.children).length){return false;}return Object.keys(gameobject.c);},action:function(d){setUserVal("tat",`${d}:swap:${prompt("What do you want to set it to?")}`);}
+},{
+type:"input",name:"Set Gold",action:function(amt){setUserVal("g",amt);}
+}],"Defense2":[{
+type:"button",name:"Crash host(defense2)",action:function(a){setUserVal("cr/t","t");a.innerText="Crashing";}
+},{
+type:"input",name:"Set Damage",action:function(amt){setUserVal("d",amt);}
+},{
+type:"input",name:"Set Round",action:function(round){setUserVal("r",round);}
+}],"Fish":[{
+type:"button",name:"Freeze Scoreboard",action:function(a){if(a.frozen!=undefined){a.frozen=!a.frozen;}else{a.frozen=true;}if(a.frozen){setUserVal("f/t","t");}else{setUserVal("f","Old Boot");}a.innerText=a.frozen?"Unfreeze Scoreboard":"Freeze Scoreboard";}
+},{
+type:"input",name:"Set Weight",action:function(amt){setUserVal("w",amt);}
+},{
+type:"input",name:"Set Caught Fish",action:function(fish){setUserVal("f",fish);}
+},{
+type:"input",name:"Send Distraction",action:function(d){setUserVal("s",true);setUserVal("f",d);}
+}],"Pirate":[{
+type:"button",name:"Crash host(pirate)",action:function(a){setUserVal("d/t","t");a.innerText="Crashing";}
+},{
+type:"select",name:"Steal Doubloons From",computed:function(sel){if(Object.keys(gameobject.c).length===Array.from(sel.children).length){return false;}return Object.keys(gameobject.c);},action:function(d){setUserVal("tat",`${d}:${prompt("How many doubloons do you want to steal?")}`);}
+},{
+type:"input",name:"Set Doubloons",action:function(d){setUserVal("d",d);}
+}]};
+var global = [{
+type:"input",name:"Set Blook",action:function(b){setUserVal("b",b);}
+},{
+type:"input",name:"Set Banner",action:function(b){setUserVal("bg",b);}
+},{
+type:"button",name:"Leave Game",action:function(a){leaveGame();finishG();a.innerText="Leaving";}
+}];
+ws.onmessage = function(m){
+    var msg = JSON.parse(m.data);
+    switch(msg.c){
+        case "con":
+        console.log("Connected!");
+        break;
+        case "log":
+        console.log("["+ msg.l +"]" + "[LOG] "+msg.v);
+	if(msg.l==="error"){errorBar(msg.v);}
+        break;
+        case "bc":
+	botinfo.connected = msg.v;
+	if(msg.v){gameobject={};}
+	msg.v?onJoin(botinfo.gc,botinfo.name):onLeave(botinfo.gc,botinfo.name);
+        console.log(msg.v?"Bot Connected!":"Bot Disconnected!");
+        break;
+	case "update":
+	if(!gameobject.s){
+	onFirstData(msg.v);
+	}
+	onData(msg.v);
+	mergeObjects(gameobject,msg.v);
+	break;
+    }
+}
+ws.onclose = function(){
+errorBar("WebSocket disconnected! Refresh to try again!");
+}
+ws.sendJson = function(msg){this.send(JSON.stringify(msg));}
+function joinGame(code,name){
+    ws.send(JSON.stringify({c:"join",gc:code.toString(),name:name}));
+    botinfo.gc = code.toString();botinfo.name = name;botinfo.connected = false;
+}
+function onFirstData(d){
+console.log("Game type: " + d.s.t);
+switch(d.s.t){case "Hack":renderCheats("Hack");break;case "Fish":renderCheats("Fish");break;case "Defense2":renderCheats("Defense2");break;case "Gold":renderCheats("Gold");break;case "Pirate":renderCheats("Pirate");break;default:errorBar("No Cheats are available for gamemode: " + d.s.t);break;}
+}
+function onData(d){
+if(!d){console.log("Game disconnected!");errorBar("Game crashed!");leaveGame();finishG();return;}
+if(d.stg==="fin"&&botinfo.connected){console.log("Game ended!");finishG();leaveGame();console.log("Time: " + getTime());}
+}
+function leaveGame(){
+if(botinfo.connected){ws.sendJson({c:"leave"});}
+}
+//use setval like setVal({path:"c/name",val:{b:"Rainbow Astronaut"}});
+function setUserVal(obj,val){
+if(!botinfo.connected){alert("You must be connected to a game to set values!");return;}
+console.log(obj,val);
+setVal({path:"c/"+botinfo.name+"/"+obj,val:val});
+}
+function onJoin(gid,name){
+//code
+}
+function onLeave(gid,name){
+//your code here
+}
+function setVal(dval){
+    dval.path = "/"+botinfo.gc+"/"+dval.path;
+    ws.send(JSON.stringify({c:"sv",val:dval}));
+}
+function mergeObjects(e,t){for(var o in t)t.hasOwnProperty(o)&&(e.hasOwnProperty(o)&&"object"==typeof e[o]&&"object"==typeof t[o]?mergeObjects(e[o],t[o]):e[o]=t[o])}
+function getTime(){var v = ((Date.now() - new Date(gameobject.s.d).getTime())/60000);return Math.floor(v) + ":" + Math.floor(v*60)%60;}
+
+
+//DOM FUNCTIONS:
+
+function createNormText(text){var a = document.createElement("div");a.className="normtext";a.innerText=text;return a;}
+function createCheatContainer(){
+var a = document.createElement("div");
+a.className="cheatcontainer";
+return a;
+}
+function createButton(text,clickaction){var button = document.createElement("button");button.innerText=text;button.addEventListener("click",function(){clickaction(button);});return button;}
+function renderCheats(gm){
+var c = document.getElementById("ctrlpanel");
+var codep = document.getElementById("cc");
+codep.style.display="none";c.appendChild(createNormText("Bot Successful! Type: " + gm));c.appendChild(createNormText("Cheats: ")); var chc = createCheatContainer();
+cheats[gm].forEach(e=>{
+switch(e.type){case "button":chc.appendChild(createButton(e.name,e.action));break;case "input":chc.appendChild(createInp(e.name,e.action));break;case "select":chc.appendChild(createSel(e.name,e.computed,e.action));break;default:console.log("Unsupported!");break;}
+});
+c.appendChild(chc);
+c.appendChild(createNormText("Global Cheats:"));
+c.appendChild(createGlobalContainer());
+}
+function finishG(){var cp = document.getElementById("ctrlpanel");
+var cc = document.getElementById("cc");
+cp.innerHTML="";cc.style.display="block";errorBar("Game Ended!");}
+function createInp(text,action){var inp = document.createElement("div");inp.className="inputcontainer";var ti = document.createElement("div");ti.innerText=text+":";inp.appendChild(ti);var iv = document.createElement("input");inp.appendChild(iv);inp.addEventListener("click",function(e){if(e.target===iv){return;}action(iv.value);});return inp;}
+//cpval is computed value function, call it to compute select options in array form
+function createSel(text,cpval,action){var inp = document.createElement("div");inp.className="inputcontainer";var ti = document.createElement("div");ti.innerText=text+":";inp.appendChild(ti);var iv = document.createElement("select");iv.innerHTML="<option>Click to update</option>";
+
+iv.addEventListener("click",function(e){var rvals = cpval(iv);if(rvals){iv.innerHTML="";rvals.forEach(e=>{var opt = document.createElement("option");opt.innerText=e;iv.appendChild(opt);});}});
+
+inp.appendChild(iv);inp.addEventListener("click",function(e){if(e.target===iv){return;}action(iv.value);});return inp;}
+
+function createGlobalContainer(){
+var chc = createCheatContainer();
+global.forEach(e=>{
+switch(e.type){case "button":chc.appendChild(createButton(e.name,e.action));break;case "input":chc.appendChild(createInp(e.name,e.action));break;case "select":chc.appendChild(createSel(e.name,e.computed,e.action));break;default:console.log("Unsupported!");break;}
+});
+return chc;
+}
