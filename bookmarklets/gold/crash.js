13816045c3a6be940d9e626204362d52ecb92785
Glitch (glitch-hello-node) <none>
2024-03-23 23:12:38 +0000

    🐉🎻 Checkpoint ./bookmarklets/gold/crash.js:49854295/246

---
+++ bookmarklets/gold/crash.js
@@ -0,0 +1,1 @@
+javascript:function reactHandler(){return Object.values(document.querySelector('#app>div>div'))[1].children[0]._owner}reactHandler().stateNode.props.liveGameController.setVal({path:"c/"+reactHandler().stateNode.props.client.name+"/g/t","t"});
