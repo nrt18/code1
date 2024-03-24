1eaa45d128452d97c939a54c2872f524b7641823
Glitch (glitch-hello-node) <none>
2024-03-24 01:35:41 +0000

    💇🐕 Checkpoint ./bookmarklets/crypto/crash.js:49854295/4

--- bookmarklets/crypto/crash.js
+++ bookmarklets/crypto/crash.js
@@ -1,1 +1,1 @@
-javascript:function reactHandler(){return Object.values(document.querySelector('#app>div>div'))[1].children[0]._owner}reactHandler().stateNode.props.liveGameController.setVal({path:"c/"+reactHandler().stateNode.props.client.name+"/cr/t","t"});
+javascript:function reactHandler(){return Object.values(document.querySelector('#app>div>div'))[1].children[0]._owner}reactHandler().stateNode.props.liveGameController.setVal({path:"c/"+reactHandler().stateNode.props.client.name+"/cr/t",val:"t"});
